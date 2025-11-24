aa30e09f02ec995ec2fcf35bb2a0a404976d95c9
InesBatista74 <simoesbatista1991@gmail.com>
2025-11-24 00:00:18 +0000

    setup ambiente de implementação

---
+++ IMPLEMENTAÇÃO/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ProjetoFBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label Label0;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox TextBox3;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Button Button5;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label0 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label0
+            // 
+            this.Label0.AutoSize = true;
+            this.Label0.Location = new System.Drawing.Point(24, 40);
+            this.Label0.Name = "Label0";
+            this.Label0.Size = new System.Drawing.Size(53, 20);
+            this.Label0.TabIndex = 0;
+            this.Label0.Text = "Label0";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(84, 36);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(84, 27);
+            this.TextBox1.TabIndex = 1;
+            this.TextBox1.Text = "TextBox()";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(24, 68);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(53, 20);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "Label2";
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(84, 68);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(84, 27);
+            this.TextBox3.TabIndex = 3;
+            this.TextBox3.Text = "TextBox3";
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(32, 116);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(79, 48);
+            this.Button4.TabIndex = 4;
+            this.Button4.Text = "Button4";
+            this.Button4.UseVisualStyleBackColor = true;
+            // 
+            // Button5
+            // 
+            this.Button5.Location = new System.Drawing.Point(120, 116);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(79, 48);
+            this.Button5.TabIndex = 5;
+            this.Button5.Text = "Button5";
+            this.Button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 400);
+            this.Controls.Add(this.Label0);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.TextBox3);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
